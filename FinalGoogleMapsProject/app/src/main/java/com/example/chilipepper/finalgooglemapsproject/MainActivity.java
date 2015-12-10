43736982d7629616578beef5156d25330954d7b7
MKlaas <klaas_mak@yahoo.com>
2015-12-09 23:35:04 -0500

    ListView On Click Additions
    Zachs Changes to main and blip for onclick

--- FinalGoogleMapsProject/app/src/main/java/com/example/chilipepper/finalgooglemapsproject/MainActivity.java
+++ FinalGoogleMapsProject/app/src/main/java/com/example/chilipepper/finalgooglemapsproject/MainActivity.java
@@ -1,11 +1,16 @@
 package com.example.chilipepper.finalgooglemapsproject;
 
+import android.app.Dialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.StrictMode;
 import android.support.v4.app.FragmentActivity;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.TabHost;
 import android.widget.TextView;
@@ -34,8 +39,16 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
     private static final String inspectionType = "insp_subtype";
     private static final String violationDescr = "violation_description";
     private static final String businessName = "business_name";
+    private static final String violationComments = "violation_comments";
     private static final String longitude = "longitude";
     private static final String latitude = "latitude";
+    private static final String address = "address";
+    private static final String city = "city";
+    private static final String state = "state";
+    private static final String recordNum = "recordnum_license";
+    private static final String zip = "postal_code";
+
+
 
     ArrayList<Blip> blipList = new ArrayList<Blip>();
     ArrayList<Marker> jsonMarkers = new ArrayList<>();
@@ -69,11 +82,16 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
 
         MapFragment mapFragment = ((MapFragment) getFragmentManager().findFragmentById(R.id.map));
         mapFragment.getMapAsync(this);
+
+
     }
+
+
 
     private void populateList() {
         ArrayAdapter<Blip> adapter = new BlipListAdapter();
         lv.setAdapter(adapter);
+
     }
 
     private class BlipListAdapter extends ArrayAdapter<Blip> {
@@ -83,23 +101,74 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
         }
 
         @Override
-        public View getView(int pos, View view, ViewGroup parent)
+        public View getView(final int pos, View view, ViewGroup parent)
         {
             if (view == null)
                 view = getLayoutInflater().inflate(R.layout.list_activity, parent, false);
 
-            Blip curBlip = blipList.get(pos);
+            final Blip curBlip = blipList.get(pos);
 
-            TextView iType = (TextView) view.findViewById(R.id.inspectionType);
+            final TextView iType = (TextView) view.findViewById(R.id.inspectionType);
             iType.setText(curBlip.get_inspectionType());
-            TextView vDesc = (TextView) view.findViewById(R.id.violationDescr);
+            final TextView vDesc = (TextView) view.findViewById(R.id.violationDescr);
             vDesc.setText(curBlip.get_violationDescr());
             TextView bName = (TextView) view.findViewById(R.id.businessName);
             bName.setText(curBlip.get_businessName());
 
+            final Dialog dialog = new Dialog(MainActivity.this);
+
+            lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                @Override
+                public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                    dialog.setContentView(R.layout.view_details);
+                    Button dialogButton = (Button) dialog.findViewById(R.id.btnCancel);
+
+                    TextView dialogName = (TextView) dialog.findViewById(R.id.txtName);
+                    dialogName.setText(curBlip.get_businessName());
+
+                    TextView dialogAddress = (TextView) dialog.findViewById(R.id.txtAddress);
+                    dialogAddress.setText(curBlip.get_address());
+
+                    TextView dialogCity = (TextView) dialog.findViewById(R.id.txtCity);
+                    dialogCity.setText(curBlip.get_city());
+
+                    TextView dialogState = (TextView) dialog.findViewById(R.id.txtState);
+                    dialogState.setText(curBlip.get_state());
+
+                    TextView dialogZip = (TextView) dialog.findViewById(R.id.txtZip);
+                    dialogZip.setText(curBlip.get_zip());
+
+                    TextView dialogRecord = (TextView) dialog.findViewById(R.id.txtRecord);
+                    dialogRecord.setText(curBlip.get_recordNum());
+
+                    TextView dialogViolation = (TextView) dialog.findViewById(R.id.txtViolation);
+                    dialogViolation.setText(curBlip.get_inspectionType());
+
+                    TextView dialogDesc = (TextView) dialog.findViewById(R.id.txtDesc);
+                    dialogDesc.setText(curBlip.get_violationDescr());
+
+                    TextView dialogComments = (TextView) dialog.findViewById(R.id.txtComments);
+                    dialogComments.setText(curBlip.get_violationComments());
+
+                    dialogButton.setText("OK");
+                    dialogButton.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            dialog.dismiss();
+                        }
+                    });
+                    dialog.show();
+                }
+            });
+
             return view;
         }
+
+
     }
+
+
 
     /**
      * Manipulates the map once available.
@@ -123,7 +192,8 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
             try {
                 JSONObject c = json.getJSONObject(i);
 
-                Blip blip = new Blip(c.getString(inspectionType), c.getString(violationDescr), c.getString(businessName),
+                Blip blip = new Blip(c.getString(inspectionType), c.getString(violationDescr), c.getString(businessName), c.getString(address),
+                        c.getString(city), c.getString(state), c.getString(recordNum), c.getString(zip), c.getString(violationComments),
                         Double.parseDouble(c.getString(longitude)), Double.parseDouble(c.getString(latitude)));
                 String crit = "CRITICAL CONTROL POINT";
 
@@ -154,7 +224,5 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
 
         populateList();
     }
-
-
 
 }
